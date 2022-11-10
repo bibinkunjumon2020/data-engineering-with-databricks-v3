60a7bccb0d28b7ec3c89fd61ffa32607ad07099d
janahab <32484112+janahab@users.noreply.github.com>
2022-11-10 15:12:53 +0100

    Create DS-lab5.py

---
+++ 05 - Workflow Jobs/DS-lab5.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %md 
+# MAGIC #DBSQL
+# MAGIC - For what pusposis is SQL workspace used?
+# MAGIC - What is purpose of SQL warehouse (formely SQL endpoint)?
+# MAGIC - You want to refresh your query every day. How do you do that?
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Your Answers**
