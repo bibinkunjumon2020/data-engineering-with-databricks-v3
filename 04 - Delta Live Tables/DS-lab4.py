7ba20a4327aff39ae61943cf25ac4fe7ce669d2e
janahab <32484112+janahab@users.noreply.github.com>
2022-11-10 15:09:16 +0100

    Create DS-lab4.py

---
+++ 04 - Delta Live Tables/DS-lab4.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #Delta live tables
+# MAGIC - What are Delta Live Tables (DLT)?
+# MAGIC - What are advantages of DLT?
+# MAGIC - You want to create delta live table. What command you use?
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Your answers**
