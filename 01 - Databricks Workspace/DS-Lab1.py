75d7161eeb8afbd4ddac2455eee41815fa69809f
janahab <32484112+janahab@users.noreply.github.com>
2022-11-10 14:52:44 +0100

    Create DS-Lab1.py

---
+++ 01 - Databricks Workspace/DS-Lab1.py
@@ -0,0 +1,119 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Can you understand DWH?
+# MAGIC  - What is it?
+# MAGIC  - What is the main use case?
+# MAGIC  - What are some of the advantages?
+# MAGIC  - What are some of the disadvantages?
+# MAGIC  - What data modeling approach is used?
+# MAGIC  [Some resources can be found here](https://www.notion.so/datasentics/Data-Warehousing-DWH-959631309517455fbd4f3259f8402084)
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC **Your answers about DWH**
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Can you understand Data Lake?
+# MAGIC  - What is it?
+# MAGIC  - What is the main use case?
+# MAGIC  - What are some of the advantages?
+# MAGIC  - What are some of the disadvantages?
+# MAGIC  - What data modeling approach is used?
+# MAGIC [Some resources can be found here](https://www.notion.so/datasentics/Data-Lake-18fa4322a4cd403c9642274b83035f47)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Your answers about Data Lake**
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Can you understand Data LakeHouse?
+# MAGIC  - What is it?
+# MAGIC  - What is the main use case?
+# MAGIC  - What are some of the advantages?
+# MAGIC  - What are some of the disadvantages?
+# MAGIC  - What data modeling approach is used?
+# MAGIC  - What data format is usually used and why? 
+# MAGIC  - How does Databricks use DataLakeHouse?
+# MAGIC Databricks documentation and videos provide a lot of resources about this
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Your answers about Data LakeHouse**
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### What are some of the problems organizations face when working with data?
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Your answers about problems with data**
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### What are the 2 main parts of Databricks(DBX) architecture
+# MAGIC - What are the components and name of the 1 parts
+# MAGIC - What are the components and name of the 2 part
+# MAGIC - What is the difference between the 2 parts
+# MAGIC - Who manages which part of the architecture
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Your answers about DBX architecture**
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Clusters
+# MAGIC - What type of clusters do we have?
+# MAGIC - What are the differences between them?
+# MAGIC - How do you specify a runtime in cluster?
+# MAGIC - What you need to do when configuration of cluster has changed?
+# MAGIC - Try to set up a cluster, play with the configuration
+# MAGIC - What is the difference between restarting, terminating and deleting the cluster?
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Your answers about clusters**
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC ### Databricks UI
+# MAGIC - What is the purpose of DataScience&Engineering?
+# MAGIC - What is the purpose of SQL Tab?
+# MAGIC - What is the purpose of Machine Learning tab
+# MAGIC - Where can you insert the github API token
+# MAGIC - Where can you change the number of spaces for tab?
+# MAGIC - Where can you manage access to databricks for other people?
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Your answers about Databricks UI**
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Notebook basics
+# MAGIC - How to run a cell, all cells above/below?
+# MAGIC - How to attach cluster, clear a state of notebook, what does clearing a state means, when you need to do it?
+# MAGIC - How to create a cells in different language?
+# MAGIC - What are some of the magic commands, how can you run a different notebook from within a notebook?
+# MAGIC - What are some of the special functions databricks makes available to you? 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Your answers about notebooks**
