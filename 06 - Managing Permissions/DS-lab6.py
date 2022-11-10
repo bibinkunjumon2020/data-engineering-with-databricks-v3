e93b3906709cd8385b4665df26888cb076b71423
janahab <32484112+janahab@users.noreply.github.com>
2022-11-10 15:11:29 +0100

    Create DS-lab6.py

---
+++ 06 - Managing Permissions/DS-lab6.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC #Managing Permissions
+# MAGIC - What is Unity Catalog?
+# MAGIC - What is order in three-layer namespace with Unity Catalog?
+# MAGIC - You are an administrator and you want to give ability to add, delete, and modify data to or from an object to user **john.doe@company.com** on the table named **cars**. What command you use?
+# MAGIC - You are an administrator and you want to give ability read access to an object to user **john.doe@company.com** on the table named **cars**. What command you use?
+# MAGIC - You are an administrator and you want to give gives all privileges to user **john.doe@company.com** on the table named **cars**. What command you use?
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC **Your Answers**
